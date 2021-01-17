495e5a7fb06f09f3d63db3ba34e8da8b3fa61a6c
yildirimGulay <gulaycontact@gmail.com>
2021-01-17 00:30:38 +0000

    Gulay's First Profile

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function changeAvatar()
+{  
+    document.getElementById("avatar").style.backgroundImage=" url('img/avataaars.png') ";
+}
+function cameback()
+{
+    document.getElementById("avatar").style.backgroundImage=" url('img/avataaars2.png') ";
+}
+
